3fde67ae156d3fd4ead8bc5d3d5d29ed69133c56
Frieder Paape, Integritee AG <frieder@integritee.network>
2022-01-04 11:41:56 +0100

    fixup! [LibOS] feat: emulate mlock family of syscalls
    Signed-off-by: Frieder Paape, Integritee AG <frieder@integritee.network>

--- LibOS/shim/src/sys/shim_mlock.c
+++ LibOS/shim/src/sys/shim_mlock.c
@@ -3,22 +3,41 @@
  *                    Frieder Paape <frieder@integritee.network>
  */
 
-#include <stddef.h>
+/*
+ * Implements mlock, mlock2, munlock, mlockall, munlockall (lock and unlock memory).
+ * These syscalls are stubbed to always return success -- Gramine cannot guarantee that
+ * the host OS will perform lock/unlock anyway, and a malicious OS can still swap pages
+ * anyway.
+ *
+ * This (dummy) functionality is required by .NET workloads.
+ */
 
 #include "shim_table.h"
 
-long shim_do_mlock(__attribute__((unused)) const void* addr, __attribute__((unused)) size_t len) {
+long shim_do_mlock(unsigned long start, size_t len) {
+    __UNUSED(start);
+    __UNUSED(len);
     return 0;
 }
 
-long shim_do_munlock(__attribute__((unused)) const void* addr, __attribute__((unused)) size_t len) {
+long shim_do_munlock(unsigned long start, size_t len) {
+    __UNUSED(start);
+    __UNUSED(len);
     return 0;
 }
 
-long shim_do_mlockall(__attribute__((unused)) int flags) {
+long shim_do_mlockall(int flags) {
+    __UNUSED(flags);
     return 0;
 }
 
 long shim_do_munlockall(void) {
+    return 0;
+}
+
+long shim_do_mlock2(unsigned long start, size_t len, int flags) {
+    __UNUSED(start);
+    __UNUSED(len);
+    __UNUSED(flags);
     return 0;
 }
